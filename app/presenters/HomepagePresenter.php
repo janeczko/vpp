3a7caf49296e1849722c829ee671e8975a60a575
Yahkem <Yahkem@users.noreply.github.com>
2015-11-29 14:02:05 +0100

    test menu

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -50,6 +50,37 @@ class HomepagePresenter extends BasePresenter
 
     public function renderTest()
     {
-        $this->render();
+		$galleryItems = $this->galleryModel->getCarouselItems();
+		$allItems = $this->galleryModel->getAllItems();
+		$imageViews = [];
+
+		if (!empty($allItems))
+		{
+			$i = 0;
+			$imageViews[$i] = [];
+
+			foreach ($allItems as $item)
+			{
+				if (count($imageViews[$i]) == self::IMAGES_PER_PAGE)
+				{
+					$i++;
+					$imageViews[$i] = [];
+				}
+
+				$imageViews[$i][] = (object) $item;
+			}
+		}
+
+		$this->render([
+			'animationTime' => (object) [
+				'balloons' => (object) [
+					'h' => 90,
+					'v' => 30
+				],
+				'gallery' => 5
+			],
+			'galleryItems' => $galleryItems,
+			'imageViews' => $imageViews
+		]);
     }
 }
